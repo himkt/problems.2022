78c0642f8658a3c5df93055ba9021e362da8eeb2
himkt <himkt@cookpad.com>
2020-08-10 14:45:26 +0900

    [Hitachi2020] Solve B

--- atcoder/hitachi2020/src/bin/b.rs
+++ atcoder/hitachi2020/src/bin/b.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+
+
 fn main() {
-    unimplemented!();
+    input! {
+        a: usize,
+        b: usize,
+        m: usize,
+        va: [i32; a],
+        vb: [i32; b],
+        x: [[i32; 3]; m]
+    }
+
+    let mut ans = va.iter().min().unwrap()
+                  + vb.iter().min().unwrap();
+
+    for xi in x {
+        let ai = xi[0] as usize;
+        let bi = xi[1] as usize;
+        let c  = xi[2];
+        ans = std::cmp::min(ans, va[ai-1] + vb[bi-1] - c);
+    }
+
+    println!("{}", ans);
 }
