5a26253809098ca12fabfcf15e524b3e63f54183
himkt <himkt@cookpad.com>
2020-08-12 20:02:26 +0900

    [AGC027] Solve A

--- atcoder/agc027/src/bin/a.rs
+++ atcoder/agc027/src/bin/a.rs
@@ -1,3 +1,29 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input!{
+        n: i64,
+        x: i64,
+        mut a: [i64; n],
+    }
+
+    let mut x = x;
+    let mut ans = 0;
+
+    a.sort();
+
+    for i in 0..n as usize {
+        x -= a[i];
+        ans += 1;
+
+        if x <= 0 {
+            break;
+        }
+    }
+
+    if x != 0 {
+        ans -= 1;
+    }
+
+    println!("{}", ans);
 }
