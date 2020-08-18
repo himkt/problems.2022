242d2a34d7eb4c3693339f97e961831640821228
himkt <himkt@cookpad.com>
2020-08-18 21:55:13 +0900

    [ABC153] Solve D

--- atcoder/abc153/src/bin/d.rs
+++ atcoder/abc153/src/bin/d.rs
@@ -1,3 +1,27 @@
+use proconio::input;
+
+
+fn solve(mut h: i64) -> i64 {
+    let mut i = 1;
+
+    loop {
+        if h > 1 {
+            h /= 2;
+            i += 1;
+        }
+        else {
+            break;
+        }
+    }
+
+    return 2i64.pow(i) - 1;
+}
+
+
 fn main() {
-    unimplemented!();
+    input! {
+        h: i64,
+    }
+
+    println!("{}", solve(h));
 }
