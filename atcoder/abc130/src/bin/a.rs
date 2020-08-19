4ebc731bda1f72b358bf1d4112c07da0308fa713
himkt <himkt@cookpad.com>
2020-08-20 00:09:49 +0900

    [ABC130] Solve A, B

--- atcoder/abc130/src/bin/a.rs
+++ atcoder/abc130/src/bin/a.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+
+
+fn solve(x: i32, a: i32) {
+    if x < a {
+        println!("0");
+    }
+    else {
+        println!("10");
+    }
+}
+
+
 fn main() {
-    unimplemented!();
+    input! {
+        x: i32,
+        a: i32,
+    }
+
+    solve(x, a);
 }
--- atcoder/abc130/src/bin/b.rs
+++ atcoder/abc130/src/bin/b.rs
@@ -1,3 +1,27 @@
+use proconio::input;
+
+
+fn solve(_: i32, x: i32, l: Vec::<i32>) {
+    let mut ans: i32 = 1;
+    let mut cur: i32 = 0;
+
+    for li in l {
+        if cur + li <= x {
+            ans += 1;
+        }
+        cur += li;
+    }
+
+    println!("{}", ans);
+}
+
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: i32,
+        x: i32,
+        l: [i32; n],
+    }
+
+    solve(n, x, l);
 }
