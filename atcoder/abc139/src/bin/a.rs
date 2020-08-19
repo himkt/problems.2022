f623afd4b66431fbed38bcbed66d0154c5a15151
himkt <himkt@cookpad.com>
2020-08-19 09:20:49 +0900

    [ABC139] Solve A

--- atcoder/abc139/src/bin/a.rs
+++ atcoder/abc139/src/bin/a.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+
+
+fn solve(s: String, t: String) {
+    let mut ans = 0;
+
+    for i in 0..3 {
+        if s[i..i+1] == t[i..i+1] {
+            ans += 1
+        }
+    }
+
+    println!("{}", ans);
+}
+
+
 fn main() {
-    unimplemented!();
+    input! {
+        s: String,
+        t: String,
+    }
+
+    solve(s, t);
 }
