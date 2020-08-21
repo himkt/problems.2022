0b9667af24437110674c92f3db7160a63c5d567e
himkt <himkt@cookpad.com>
2020-08-21 21:27:02 +0900

    [ABC155] Solve C

--- atcoder/abc155/src/bin/c.rs
+++ atcoder/abc155/src/bin/c.rs
@@ -1,3 +1,35 @@
+use proconio::input;
+
+
+fn solve(s: Vec::<String>) {
+    let mut c = std::collections::HashMap::new();
+
+    for si in &s {
+        *c.entry(si).or_insert(1) += 1;
+    }
+
+    let max = c.values().max().unwrap();
+    let mut ns = Vec::new();
+
+    for (k, v) in &c {
+        if v == max {
+            ns.push(k);
+        }
+    }
+
+    ns.sort();
+
+    for si in ns {
+        println!("{}", si);
+    }
+}
+
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        s: [String; n],
+    }
+
+    solve(s);
 }
