e59d1ed8c1f6ae35ee5c4f00bb5b96878638a859
himkt <himkt@cookpad.com>
2020-08-22 23:23:16 +0900

    [AGC024] Solve A

--- atcoder/agc024/src/bin/a.rs
+++ atcoder/agc024/src/bin/a.rs
@@ -1,3 +1,23 @@
+use proconio::input;
+
+
+fn solve(a: i128, b: i128, _: i128, k: i128) {
+    if k % 2 == 0 {
+        println!("{}", a-b);
+    }
+    else {
+        println!("{}", b-a);
+    }
+}
+
+
 fn main() {
-    unimplemented!();
+    input! {
+        a: i128,
+        b: i128,
+        c: i128,
+        k: i128,
+    }
+
+    solve(a, b, c, k);
 }
