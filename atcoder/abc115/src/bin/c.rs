7898ffc6ee0eb203cede06c987d2fa59a581caa8
himkt <himkt@cookpad.com>
2020-11-10 00:53:51 +0900

    [ABC115] Solve C

--- atcoder/abc115/src/bin/c.rs
+++ atcoder/abc115/src/bin/c.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+
+
+fn solve(n: usize, k: usize, mut h: Vec<i32>) {
+    h.sort();
+
+    let mut dp = vec![0; n-k+1];
+
+    for i in 0..n-k+1 {
+        dp[i] = h[i+k-1] - h[i];
+    }
+
+    println!("{}", dp.iter().min().unwrap());
+}
+
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        k: usize,
+        h: [i32; n],
+    }
+
+    solve(n, k, h);
 }
