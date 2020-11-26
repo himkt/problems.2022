5a96184a3cb57fd68eec1514151ca365e70f0bc0
himkt <himkt@cookpad.com>
2020-11-26 23:41:42 +0900

    [AGC021] Solve A

--- atcoder/agc021/src/bin/a.rs
+++ atcoder/agc021/src/bin/a.rs
@@ -1,3 +1,35 @@
+use proconio::input;
+
+
+fn solve (n: i64) {
+    let s = n.to_string();
+
+    let uniq: std::collections::HashSet<i32> = s
+        .chars()
+        .map(|c| c as i32 - '0' as i32)
+        .collect();
+
+    let head_num = s.chars().next().unwrap() as i32 - '0' as i32;
+    let spec: std::collections::HashSet<i32> = [9, head_num]
+        .iter()
+        .copied()
+        .collect();
+
+    if s.len() == 1 {
+        println!("{}", head_num);
+    }
+    else if uniq == spec {
+        println!("{}", head_num + (s.len() as i32 - 1) * 9);
+    }
+    else {
+        println!("{}", (head_num - 1) + (s.len() as i32 - 1) * 9);
+    }
+}
+
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: i64,
+    }
+    solve(n);
 }
