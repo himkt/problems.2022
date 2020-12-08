0afd1d017c1c8d6fb862215961890692b5527857
himkt <himkt@cookpad.com>
2020-12-09 00:17:19 +0900

    [ABC106] Solve C

--- atcoder/abc106/src/bin/c.rs
+++ atcoder/abc106/src/bin/c.rs
@@ -1,3 +1,30 @@
+use proconio::input;
+
+
+fn solve (s: Vec<char>, k: i128) {
+    if k == 1 {
+        println!("{}", s[0]);
+        return
+    }
+
+    let mut i = 1;
+    for c in s {
+        if c != '1' || i == k {
+            println!("{}", c);
+            return
+        }
+        i += 1;
+    }
+
+    println!("1");
+}
+
+
 fn main() {
-    unimplemented!();
+    input! {
+        s: String,
+        k: i128,
+    }
+
+    solve(s.chars().collect(), k);
 }
