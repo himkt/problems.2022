daeea92685dffdf6302b7f5ba79c8844c610ffac
himkt <himkt@cookpad.com>
2020-12-08 23:03:19 +0900

    [AGC041] Solve A

--- atcoder/agc041/src/bin/a.rs
+++ atcoder/agc041/src/bin/a.rs
@@ -1,3 +1,27 @@
+use proconio::input;
+
+
+fn solve(n: i128, a: i128, b: i128) {
+    let da = a - 1;
+    let db = n - b;
+    let dab = b - a;
+
+    let mut ans = (dab + 1) / 2;
+
+    if dab % 2 != 0 {
+        ans += std::cmp::min(da, db);
+    }
+
+    println!("{}", ans);
+}
+
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: i128,
+        a: i128,
+        b: i128,
+    }
+
+    solve(n, a, b);
 }
