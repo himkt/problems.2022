e4de5a2185b209499b1277d94ee1b3522f09d5c4
himkt <himkt@cookpad.com>
2020-12-22 21:02:37 +0900

    [DDCC2020] Solve B

--- atcoder/ddcc2020-qual/src/bin/b.rs
+++ atcoder/ddcc2020-qual/src/bin/b.rs
@@ -1,3 +1,32 @@
+
+use proconio::input;
+
+
+fn solve(a: Vec<i64>) {
+    let total: i64 = a.iter().sum();
+
+    let mut cur = 0;
+    let mut left = 0;
+
+    for a_i in &a {
+        if left + a_i > total / 2 {
+            break;
+        }
+
+        left += a_i;
+        cur += 1;
+    }
+
+    let right = total - left - a[cur];
+    let ans = (std::cmp::max(left, right) - (std::cmp::min(left, right) + a[cur])).abs();
+    println!("{}", ans);
+}
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: i64,
+        a: [i64; n],
+    }
+
+    solve(a);
 }
