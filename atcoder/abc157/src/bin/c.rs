b98ae83bd9e607d2ed2e257d382bbdeafccfeba9
Makoto Hiramatsu <himkt@cookpad.com>
2021-05-19 22:08:59 +0900

    [ABC157] Solve C

--- atcoder/abc157/src/bin/c.rs
+++ atcoder/abc157/src/bin/c.rs
@@ -1,3 +1,54 @@
+use proconio::input;
+
+
+fn solve(n: usize, sc: Vec<(usize, i32)>) -> i32 {
+    let mut nums = Vec::<i32>::new();
+    nums.resize(n, -1);
+
+    for (si, ci) in sc.iter() {
+        // specify 0 for the first digit
+        if *si == 1 && *ci == 0 && n != 1 {
+            return -1;
+        }
+
+        if nums[*si-1] == -1 {
+            nums[*si-1] = *ci;
+        }
+        else {
+            if nums[*si-1] == *ci {
+                continue;
+            }
+            return -1;
+        }
+    }
+
+    for i in 0..n {
+        if nums[i] == -1 {
+            if i == 0 && n > 1 {
+                nums[i] = 1;
+            }
+            else {
+                nums[i] = 0;
+            }
+        }
+    }
+
+    let mut ans = 0;
+    for i in 0..n {
+        let base = (10 as i32).pow((n - i - 1) as u32);
+        ans += nums[i] * base;
+    }
+
+    ans
+}
+
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        m: usize,
+        sc: [(usize, i32); m],
+    }
+
+    println!("{}", solve(n, sc));
 }
