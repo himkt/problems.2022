7bae07bf12c01c3552f121ceb47ed98c4f3a3c07
himkt <himkt@klis.tsukuba.ac.jp>
2021-08-08 00:10:21 +0900

    [DP] Solve D

--- atcoder/dp/src/bin/d.rs
+++ atcoder/dp/src/bin/d.rs
@@ -1,12 +1,28 @@
 use proconio::input;
 
 
-fn solve() {
-}
-
-
-fn main() {
+fn main () {
     input! {
+        n: usize,
+        w: usize,
+        wv: [(usize, usize); n],
     }
-    solve();
+
+    let ws: Vec<usize> = wv.iter().map(|pair| pair.0).collect();
+    let vs: Vec<usize> = wv.iter().map(|pair| pair.1).collect();
+
+    let mut dp = vec![vec![0; w+1]; n+1];
+
+    for i in 0..n {
+        for j in 0..=w {
+            if j < ws[i] {
+                dp[i+1][j] = dp[i][j];
+            }
+            else {
+                dp[i+1][j] = dp[i][j].max(dp[i][j-ws[i]] + vs[i]);
+            }
+        }
+    }
+
+    println!("{}", dp[n][w]);
 }
