91d56927e2d76b637764aef662d8bf83cb72dd53
himkt <himkt@klis.tsukuba.ac.jp>
2021-08-11 20:21:47 +0900

    [DP] Solve E

--- atcoder/dp/src/bin/e.rs
+++ atcoder/dp/src/bin/e.rs
@@ -1,12 +1,37 @@
 use proconio::input;
 
 
-fn solve() {
-}
-
-
-fn main() {
+fn main () {
     input! {
+        n: usize,
+        w: usize,
+        wv: [(usize, usize); n],
     }
-    solve();
+
+    let ws: Vec<usize> = wv.iter().map(|pair| pair.0).collect();
+    let vs: Vec<usize> = wv.iter().map(|pair| pair.1).collect();
+
+    const INF: usize = 1001001001;
+    let mut dp = vec![vec![INF; n * 1000 + 1]; n + 1];
+    dp[0][0] = 0;
+
+    for i in 0..n {
+        for j in 0..=(n * 1000) {
+            if j < vs[i] {
+                dp[i+1][j] = dp[i][j];
+            }
+            else {
+                dp[i+1][j] = dp[i][j].min(dp[i][j-vs[i]] + ws[i]);
+            }
+        }
+    }
+
+    let mut ans = 0;
+    for j in 0..=(n * 1000) {
+        if dp[n][j] <= w {
+            ans = j;
+        }
+    }
+
+    println!("{}", ans);
 }
