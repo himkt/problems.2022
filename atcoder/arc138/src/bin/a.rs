66d211541f48a00c7fe9314afa9d0d50897f68d9
Makoto Hiramatsu <himkt@klis.tsukuba.ac.jp>
2022-05-04 19:59:50 +0900

    [ARC138] Refactor a

--- atcoder/arc138/src/bin/a.rs
+++ atcoder/arc138/src/bin/a.rs
@@ -2,26 +2,20 @@ const INF: usize = 1_000_000_007;
 
 
 pub fn lower_bound(range: std::ops::Range<usize>, prop: &dyn Fn(usize) -> bool) -> usize {
-    if prop(range.start) {
-        range.start
-    }
-    else {
-        let mut ng = range.start;
-        let mut ok = range.end;
+    if prop(range.start) { return range.start; }
 
-        while ok - ng > 1 {
-            let middle = ng + (ok - ng) / 2;
+    let mut ng = range.start;
+    let mut ok = range.end;
 
-            if prop(middle) {
-                ok = middle;
-            }
-            else {
-                ng = middle;
-            }
+    while ok - ng > 1 {
+        let middle = ng + (ok - ng) / 2;
+        match prop(middle) {
+            true => { ok = middle },
+            false => { ng = middle },
         }
-
-        ok
     }
+
+    ok
 }
 
 
