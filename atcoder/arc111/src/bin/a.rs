f0540d98552e4c8ca66fa3c0b242d8724fab6d34
Makoto Hiramatsu <himkt@klis.tsukuba.ac.jp>
2022-05-22 21:25:23 +0900

    [ARC111] Solve A

---
+++ atcoder/arc111/src/bin/a.rs
@@ -0,0 +1,58 @@
+pub fn modpow(mut a: usize, mut n: usize, m: usize) -> usize {
+    let mut ans = 1;
+
+    while n > 0 {
+        if n & 1 == 1 {
+            ans = ans * a % m;
+        }
+
+        a = a * a % m;
+        n >>= 1;
+    }
+
+    ans
+}
+
+
+#[allow(clippy::needless_range_loop)]
+fn main() {
+    let mut scanner = Scanner::new();
+    let n: usize = scanner.cin();
+    let m: usize = scanner.cin();
+    let p = modpow(10, n, m * m);
+    println!("{}", p / m);
+}
+
+
+use std::collections::VecDeque;
+use std::io::{self, Write};
+use std::str::FromStr;
+
+pub struct Scanner {
+    stdin: io::Stdin,
+    buffer: VecDeque<String>,
+}
+impl Scanner {
+    fn new() -> Self {
+        Self { stdin: io::stdin(), buffer: VecDeque::new() }
+    }
+
+    pub fn cin<T: FromStr>(&mut self) -> T {
+        while self.buffer.is_empty() {
+            let mut line = String::new();
+            let _ = self.stdin.read_line(&mut line);
+            for w in line.split_whitespace() {
+                self.buffer.push_back(String::from(w));
+            }
+        }
+        self.buffer.pop_front().unwrap().parse::<T>().ok().unwrap()
+    }
+
+    pub fn vec<T: FromStr>(&mut self, n: usize) -> Vec<T> {
+        (0..n).map(|_| self.cin()).collect()
+    }
+}
+
+pub fn flush() {
+    std::io::stdout().flush().unwrap();
+}
