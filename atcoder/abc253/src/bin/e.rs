64e7c75babc26d5e46bc713bd3985b8e2fbc24ef
Makoto Hiramatsu <himkt@klis.tsukuba.ac.jp>
2022-05-29 17:02:35 +0900

    [ABC253] Solve E

--- atcoder/abc253/src/bin/e.rs
+++ atcoder/abc253/src/bin/e.rs
@@ -1,3 +1,22 @@
+const DIV: usize = 998244353;
+
+
+pub fn modpow(mut a: usize, mut n: usize, m: usize) -> usize {
+    let mut ans = 1;
+
+    while n > 0 {
+        if n & 1 == 1 {
+            ans = ans * a % m;
+        }
+
+        a = a * a % m;
+        n >>= 1;
+    }
+
+    ans
+}
+
+
 #[allow(clippy::needless_range_loop)]
 fn main() {
     let mut scanner = Scanner::new();
@@ -6,19 +25,46 @@ fn main() {
     let m: usize = scanner.cin();
     let k: usize = scanner.cin();
 
-    let mut dp = vec![vec![0; m]; n];
-    // for ni in 1..=n {
-        let ni = 1;
-        dp[0][ni] = 1;
+    if k == 0 {
+        let ans = modpow(m, n, DIV);
+        println!("{}", ans);
+        return;
+    }
 
-        for i in 1..n {
-            for ki in 0..k {
-                dp[i][ni+ki] += dp[i-1][ni];
-            }
+    let mut dp = vec![vec![0; m]; n];
+    for j in 0..m {
+        dp[0][j] = 1;
+    }
+
+    for i in 1..n {
+        let mut cum = vec![0; m];
+        cum[0] = dp[i-1][0];
+        for j in 1..m {
+            cum[j] = dp[i-1][j] + cum[j-1];
         }
 
-        println!("{:?}", dp);
-    // }4
+        for j in 0..m {
+            let mut v = cum[m-1];
+
+            let r = (j + k - 1).min(m - 1);
+            v -= cum[r];
+
+            if j >= k {
+                let l = j - k;
+                v += cum[l];
+            }
+
+            dp[i][j] = v % DIV;
+        }
+    }
+
+    let mut ans = 0;
+    for &v in dp[n-1].iter() {
+        ans += v;
+        ans %= DIV;
+    }
+
+    println!("{}", ans);
 }
 
 
