9291f32b5f65ae87040719e4d1353c2533277c42
Makoto Hiramatsu <himkt@klis.tsukuba.ac.jp>
2022-06-02 00:59:59 +0900

    [ARC141] Solve B

--- atcoder/arc141/src/bin/b.rs
+++ atcoder/arc141/src/bin/b.rs
@@ -1,8 +1,49 @@
+const DIV: usize = 998244353;
+
+
 #[allow(clippy::needless_range_loop)]
 fn main() {
     let mut scanner = Scanner::new();
     let n: usize = scanner.cin();
     let m: usize = scanner.cin();
+
+    if n > 60 {
+        println!("0");
+        return;
+    }
+
+    let mut dp = vec![vec![0; 61]; n];
+    dp[0][1] = 1;
+
+    let mut p = 2;  // p=2^(k-1)
+    for k in 2..61 {
+        if p > m {
+            dp[0][k] = 0;
+        }
+        else {
+            let q = 2 * p;  // q=2^k
+            dp[0][k] = (q - 1).min(m) - p + 1;
+            dp[0][k] %= DIV;
+        }
+        p *= 2;
+    }
+
+    for i in 1..n {
+        for k in 1..61 {
+            for j in 1..k {
+                dp[i][k] += dp[0][k] * dp[i-1][j];
+                dp[i][k] %= DIV;
+            }
+        }
+    }
+
+    let mut ans = 0;
+    for j in 0..61 {
+        ans += dp[n-1][j];
+        ans %= DIV;
+    }
+
+    println!("{}", ans);
 }
 
 
