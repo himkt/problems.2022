cc4a7667c39bc1abfed0933c3f141f593695e684
himkt <himkt@klis.tsukuba.ac.jp>
2022-06-18 00:21:30 +0900

    [ABC194] Solve D

--- atcoder/abc194/src/bin/d.rs
+++ atcoder/abc194/src/bin/d.rs
@@ -1,50 +1,36 @@
 #[allow(clippy::needless_range_loop)]
 fn main() {
     let mut scanner = Scanner::new();
-}
+    let n: usize = scanner.cin();
 
-
-use std::collections::VecDeque;
-use std::io::{self, Write};
-use std::str::FromStr;
-
-#[allow(dead_code)]
-struct Scanner {
-    stdin: io::Stdin,
-    buffer: VecDeque<String>,
-}
-#[allow(dead_code)]
-impl Scanner {
-    fn new() -> Self {
-        Self { stdin: io::stdin(), buffer: VecDeque::new() }
+    let mut ans: f64 = 0.0;
+    for i in 2..=n {
+        let nn: f64 = n as f64;
+        let ii: f64 = i as f64;
+        ans += nn / (nn - ii + 1.0);
     }
 
-    fn cin<T: FromStr>(&mut self) -> T {
+    println!("{}", ans);
+}
+
+use std::io::Write;
+pub fn flush() { std::io::stdout().flush().unwrap(); }
+pub struct Scanner { stdin: std::io::Stdin, buffer: std::collections::VecDeque<String> }
+impl Scanner {
+    fn new() -> Self {
+        Self { stdin: std::io::stdin(), buffer: std::collections::VecDeque::new() }
+    }
+
+    pub fn cin<T: std::str::FromStr>(&mut self) -> T {
         while self.buffer.is_empty() {
             let mut line = String::new();
             let _ = self.stdin.read_line(&mut line);
-            for w in line.split_whitespace() {
-                self.buffer.push_back(String::from(w));
-            }
+            line.split_whitespace().for_each(|x| self.buffer.push_back(String::from(x)));
         }
         self.buffer.pop_front().unwrap().parse::<T>().ok().unwrap()
     }
 
-    fn vec<T: FromStr>(&mut self, n: usize) -> Vec<T> {
+    pub fn vec<T: std::str::FromStr>(&mut self, n: usize) -> Vec<T> {
         (0..n).map(|_| self.cin()).collect()
     }
-}
-
-#[allow(dead_code)]
-fn flush() {
-    std::io::stdout().flush().unwrap();
-}
-
-#[macro_export]
-macro_rules! trace {
-    ($x:expr) => {
-        #[cfg(debug_assertions)]
-        eprintln!(">>> {} = {:?}", stringify!($x), $x)
-    };
-    ($($xs:expr),*) => { trace!(($($xs),*)) }
 }
