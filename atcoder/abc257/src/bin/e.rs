445f5d2b3f9b44c04b2376d1b1edba24dc257cf2
himkt <himkt@klis.tsukuba.ac.jp>
2022-06-26 17:54:54 +0900

    [ABC257] Solve E

--- atcoder/abc257/src/bin/e.rs
+++ atcoder/abc257/src/bin/e.rs
@@ -1,9 +1,64 @@
 #[allow(clippy::needless_range_loop)]
 fn main() {
     let mut scanner = Scanner::new();
+    let n: usize = scanner.cin();
+    let m: usize = 9;
+
+    let mut c: Vec<(usize, usize)> = (1..=m)
+        .map(|i| {
+            let ci: usize = scanner.cin();
+            (ci, i)
+        })
+        .collect();
+    c.sort_unstable_by_key(|&(ci, i)| (ci, Reverse(i)));
+
+    let mut cc = vec![c[0]];
+    for i in 1..m {
+        if c[i].0 == cc.last().unwrap().0 {
+            continue;
+        }
+        cc.push(c[i]);
+    }
+
+    let mut c = cc;
+    let (c0, i0) = c[0];
+
+    let mut vs = vec![];
+    let mut xs = vec![];
+    let mut sum = 0;
+    while sum + c0 <= n {
+        vs.push(i0);
+        xs.push(c0);
+        sum += c0;
+    }
+
+    c.sort_unstable_by_key(|&(_, i)| Reverse(i));
+
+    let p = vs.len();
+    let m = c.len();
+
+    for k in 0..p {
+        for q in 0..m {
+            let (cj, j) = c[q];
+            if xs[k] > cj {
+                continue;
+            }
+
+            let diff = cj - c0;
+            if sum + diff <= n {
+                vs[k] = j;
+                xs[k] = cj;
+                sum += diff;
+                break;
+            }
+        }
+    }
+
+    let ans: String = vs.iter().map(|x| x.to_string()).collect::<Vec<String>>().join("");
+    println!("{}", ans);
 }
 
-use std::io::Write;
+use std::{io::Write, cmp::Reverse};
 pub fn flush() { std::io::stdout().flush().unwrap(); }
 pub struct Scanner { stdin: std::io::Stdin, buffer: std::collections::VecDeque<String> }
 impl Scanner {
