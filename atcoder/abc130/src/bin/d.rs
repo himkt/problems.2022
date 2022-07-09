f20940bb3d2b29d5b3d43b86d8075a4cd4aec4ec
himkt <himkt@klis.tsukuba.ac.jp>
2022-07-09 13:49:24 +0900

    [ABC130] Solve D

--- atcoder/abc130/src/bin/d.rs
+++ atcoder/abc130/src/bin/d.rs
@@ -1,3 +1,60 @@
+pub fn lower_bound(range: std::ops::Range<usize>, prop: &dyn Fn(usize) -> bool) -> usize {
+    if prop(range.start) {
+        return range.start;
+    }
+
+    let mut ng = range.start;
+    let mut ok = range.end;
+
+    while ok - ng > 1 {
+        let middle = ng + (ok - ng) / 2;
+        match prop(middle) {
+            true => ok = middle,
+            false => ng = middle,
+        }
+    }
+
+    ok
+}
+
+#[allow(clippy::needless_range_loop)]
 fn main() {
-    unimplemented!();
+    let mut scanner = Scanner::new();
+    let n: usize = scanner.cin();
+    let k: i64 = scanner.cin();
+
+    let mut a: Vec<i64> = scanner.vec(n);
+    for i in 1..n {
+        a[i] += a[i - 1];
+    }
+
+    let mut ans = 0;
+    let li = lower_bound(0..n, &|x| a[x] >= k);
+
+    if li == n {
+        println!("{}", ans);
+        return;
+    }
+
+    ans += n - li;
+
+    for i in 1..n {
+        let li = lower_bound(i..n, &|x| a[x] >= k + a[i - 1]);
+        ans += n - li;
+    }
+
+    println!("{}", ans);
+}
+
+use std::io::Write; pub fn flush() { std::io::stdout().flush().unwrap(); }
+pub struct Scanner { buffer: std::collections::VecDeque<String>, buf: String }
+impl Scanner {
+    pub fn new() -> Self { Scanner { buffer: std::collections::VecDeque::new(), buf: String::new() } }
+    pub fn cin<T: std::str::FromStr>(&mut self) -> T {
+        if !self.buffer.is_empty() { return self.buffer.pop_front().unwrap().parse::<T>().ok().unwrap(); }
+        self.buf.truncate(0); std::io::stdin().read_line(&mut self.buf).ok();
+        self.buf.to_owned().split_whitespace().for_each(|x| self.buffer.push_back(String::from(x)));
+        self.buffer.pop_front().unwrap().parse::<T>().ok().unwrap()
+    }
+    pub fn vec<T: std::str::FromStr>(&mut self, n: usize) -> Vec<T> { (0..n).map(|_| self.cin()).collect() }
 }
