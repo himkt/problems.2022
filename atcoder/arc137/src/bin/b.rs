cf2b855d32c83ee41c539037c50474e1d0aed090
himkt <himkt@klis.tsukuba.ac.jp>
2022-07-24 20:38:42 +0900

    [ARC137] re: Solve B (using unified segment tree)

--- atcoder/arc137/src/bin/b.rs
+++ atcoder/arc137/src/bin/b.rs
@@ -1,93 +1,173 @@
 #[derive(Debug, Clone)]
 pub struct SegmentTree {
     v: Vec<i64>,
-    op: Op,
+    mode: Mode,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum Mode {
+    RangeUpdate(Op),
+    RangeGet(Op),
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Op {
     Max,
     Min,
     Add,
 }
 
+// Segment tree implementation. All operations are 0-origin.
+// Note that a half-open interval [l, r) is used as a range representation.
 impl SegmentTree {
-    const SEQ_LEN: usize = 1<<20;
-    const MAX: i64 =   1_000_000_000_000;
-    const MIN: i64 = - 1_000_000_000_000;
+    const SEQ_LEN: usize = 1 << 20;
+    const MAX: i64 = 1_000_000_000_000;
+    const MIN: i64 = -1_000_000_000_000;
 
-    pub fn new(op: Op) -> Self {
-        let default = match &op {
-            Op::Add => { 0 },
-            Op::Max => { SegmentTree::MIN },
-            Op::Min => { SegmentTree::MAX },
+    pub fn new(mode: Mode) -> Self {
+        let default = match &mode {
+            Mode::RangeGet(op) => match op {
+                Op::Add => 0,
+                Op::Max => SegmentTree::MIN,
+                Op::Min => SegmentTree::MAX,
+            },
+            Mode::RangeUpdate(op) => match op {
+                Op::Add => 0,
+                Op::Max => SegmentTree::MIN,
+                Op::Min => SegmentTree::MAX,
+            },
         };
 
         Self {
             v: vec![default; 2 * SegmentTree::SEQ_LEN],
-            op,
+            mode,
+        }
+    }
+
+    /// Get an i-th element of from the tree.
+    pub fn get_one(&mut self, mut index: usize) -> i64 {
+        index += SegmentTree::SEQ_LEN;
+
+        let mut sum = 0;
+        sum += self.v[index];
+
+        while index > 0 {
+            index /= 2;
+            sum += self.v[index];
+        }
+
+        sum
+    }
+
+    /// Run a range query.
+    pub fn get_range(&self, l: usize, r: usize) -> i64 {
+        fn _get_range(
+            op: &Op,
+            v: &Vec<i64>,
+            ql: usize,
+            qr: usize,
+            sl: usize,
+            sr: usize,
+            pos: usize,
+        ) -> i64 {
+            if qr <= sl || sr <= ql {
+                return match &op {
+                    Op::Add => 0,
+                    Op::Max => SegmentTree::MIN,
+                    Op::Min => SegmentTree::MAX,
+                };
+            }
+            if ql <= sl && sr <= qr {
+                return v[pos];
+            }
+
+            let sm = (sl + sr) / 2;
+            let lv = _get_range(op, v, ql, qr, sl, sm, pos * 2);
+            let rv = _get_range(op, v, ql, qr, sm, sr, pos * 2 + 1);
+
+            match &op {
+                Op::Add => lv + rv,
+                Op::Max => lv.max(rv),
+                Op::Min => lv.min(rv),
+            }
+        }
+
+        if let Mode::RangeGet(op) = &self.mode {
+            _get_range(op, &self.v, l, r, 0, SegmentTree::SEQ_LEN, 1)
+        } else {
+            panic!("Unsupported");
         }
     }
 
     /// Update an i-th element to `value`.
-    /// 0-origin.
     pub fn update_one(&mut self, mut index: usize, value: i64) {
         index += SegmentTree::SEQ_LEN;
 
-        match &self.op {
-            Op::Add => {
+        match &self.mode {
+            Mode::RangeGet(Op::Add) => {
                 self.v[index] += value;
-            },
-            _ => {
+            }
+            Mode::RangeGet(Op::Max) => {
                 self.v[index] = value;
             }
+            Mode::RangeGet(Op::Min) => {
+                self.v[index] = value;
+            }
+            _ => panic!("Unsupported"),
         };
 
-        while index > 0 {
-            index /= 2;
-            let lv = self.v[index * 2];
-            let rv = self.v[index * 2 + 1];
+        if let Mode::RangeGet(op) = &self.mode {
+            while index > 0 {
+                index /= 2;
+                let lv = self.v[index * 2];
+                let rv = self.v[index * 2 + 1];
 
-            match &self.op {
-                Op::Add => {
-                    self.v[index] = lv + rv;
+                match op {
+                    Op::Add => {
+                        self.v[index] = lv + rv;
+                    }
+                    Op::Max => {
+                        self.v[index] = lv.max(rv);
+                    }
+                    Op::Min => {
+                        self.v[index] = lv.min(rv);
+                    }
+                };
+            }
+        }
+    }
+
+    /// Add `value` to the range `[l, r)`.
+    pub fn update_range(&mut self, mut l: usize, mut r: usize, value: i64) {
+        if let Mode::RangeUpdate(op) = &self.mode {
+            l += SegmentTree::SEQ_LEN;
+            r += SegmentTree::SEQ_LEN;
+
+            while l < r {
+                if l % 2 == 1 {
+                    match op {
+                        Op::Add => {
+                            self.v[l] += value;
+                            l += 1;
+                        }
+                        _ => panic!("Unsupported"),
+                    }
                 }
-                Op::Max => {
-                    self.v[index] = lv.max(rv);
-                },
-                Op::Min => {
-                    self.v[index] = lv.min(rv);
-                },
-            };
-        }
-    }
+                l /= 2;
 
-    /// Run a range query on `[l, r)`. Note that it is a half-open interval.
-    /// 0-origin.
-    pub fn op(&self, l: usize, r: usize) -> i64 {
-        self._op(l, r, 0, SegmentTree::SEQ_LEN, 1)
-    }
-
-    fn _op(&self, ql: usize, qr: usize, sl: usize, sr: usize, pos: usize) -> i64 {
-        if qr <= sl || sr <= ql {
-            return match &self.op {
-                Op::Add => 0,
-                Op::Max => SegmentTree::MIN,
-                Op::Min => SegmentTree::MAX,
-            };
-        }
-        if ql <= sl && sr <= qr {
-            return self.v[pos];
-        }
-
-        let sm = (sl + sr) / 2;
-        let lv = self._op(ql, qr, sl, sm, pos * 2);
-        let rv = self._op(ql, qr, sm, sr, pos * 2 + 1);
-
-        match &self.op {
-            Op::Add => { lv + rv },
-            Op::Max => { lv.max(rv) },
-            Op::Min => { lv.min(rv) },
+                if r % 2 == 1 {
+                    match op {
+                        Op::Add => {
+                            self.v[r - 1] += value;
+                            r -= 1;
+                        }
+                        _ => panic!("Unsupported"),
+                    }
+                }
+                r /= 2;
+            }
+        } else {
+            panic!("Unsupported");
         }
     }
 }
@@ -108,8 +188,8 @@ fn main() {
         cum[i] += cum[i - 1];
     }
 
-    let mut rminq = SegmentTree::new(Op::Min);
-    let mut rmaxq = SegmentTree::new(Op::Max);
+    let mut rminq = SegmentTree::new(Mode::RangeGet(Op::Min));
+    let mut rmaxq = SegmentTree::new(Mode::RangeGet(Op::Max));
 
     for i in 0..n {
         rminq.update_one(i, cum[i]);
@@ -121,11 +201,11 @@ fn main() {
 
     for i in (0..n).rev() {
         let lv = cum[i];
-        let rv = rminq.op(0, i + 1);
+        let rv = rminq.get_range(0, i + 1);
         max = max.max(lv - rv);
 
         let lv = cum[i];
-        let rv = rmaxq.op(0, i + 1);
+        let rv = rmaxq.get_range(0, i + 1);
         min = min.min(lv - rv);
     }
 
