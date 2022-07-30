ea06c86c8b2157ba42baa51ca71aed03e203dcf4
himkt <himkt@klis.tsukuba.ac.jp>
2022-07-30 20:45:35 +0900

    [Tenka1Beginner2018] re: Solve C

--- atcoder/tenka1-2018-beginner/src/bin/c.rs
+++ atcoder/tenka1-2018-beginner/src/bin/c.rs
@@ -1,108 +1,78 @@
+fn sign(i: usize) -> i64 {
+    if i % 2 == 0 {
+        return 1;
+    }
+    -1
+}
+
+#[allow(clippy::needless_range_loop)]
+fn test(n: usize, a1: &[usize], a2: &[usize]) -> usize {
+    let n_i64 = n as i64;
+    let c: i64 = n as i64 / 2;
+
+    let mut arr: Vec<usize> = vec![0; n];
+    arr[c as usize] = a1[0];
+
+    for i in 1..n {
+        let i_i64 = i as i64;
+        let x = (i_i64 + 1) / 2;
+        let d = sign(i) * (2 * x - 1);
+
+        if c + d < 0 || c + d > n_i64 - 1 {
+            break;
+        }
+
+        let ni = (c + d) as usize;
+        arr[ni] = a2[i - 1];
+    }
+
+    for i in 1..n {
+        let i_i64 = i as i64;
+        let x = (i_i64 + 1) / 2;
+        let d = sign(i) * 2 * x;
+
+        if c + d < 0 || c + d > n_i64 - 1 {
+            break;
+        }
+
+        let ni = (c + d) as usize;
+        arr[ni] = a1[i];
+    }
+
+    let mut ans = 0;
+    for i in 1..n {
+        ans += arr[i-1].max(arr[i]) - arr[i-1].min(arr[i]);
+    }
+
+    ans
+}
+
+#[allow(clippy::needless_range_loop)]
 fn main() {
     let mut scanner = Scanner::new();
     let n: usize = scanner.cin();
-    let mut a: Vec<i64> = scanner.vec(n);
-    a.sort_unstable();
+    let mut a_asc: Vec<usize> = (0..n)
+        .map(|_| scanner.cin::<usize>())
+        .collect();
+    a_asc.sort_unstable();
 
-    let mut deque1: VecDeque<i64> = VecDeque::from(a);
-    let mut deque2: VecDeque<i64> = deque1.clone();
-    let mut b1: Vec<i64> = vec![0; n];
-    let mut b2: Vec<i64> = vec![0; n];
+    let mut a_dsc: Vec<usize> = a_asc.to_vec();
+    a_dsc.reverse();
 
-    let c = n / 2;
-    b1[c] = deque1.pop_front().unwrap();
-
-    for i in 1..=n {
-        if i % 2 == 1 {
-            if i <= c {
-                b1[c-i] = deque1.pop_back().unwrap();
-            }
-            if c + i < n {
-                b1[c+i] = deque1.pop_back().unwrap();
-            }
-        }
-        else {
-            if i <= c {
-                b1[c-i] = deque1.pop_front().unwrap();
-            }
-            if c + i < n {
-                b1[c+i] = deque1.pop_front().unwrap();
-            }
-        }
-    }
-
-    let mut ans1: i64 = 0;
-    for i in 0..n-1 {
-        ans1 += (b1[i] - b1[i+1]).abs();
-    }
-
-    b2[c] = deque2.pop_back().unwrap();
-
-    for i in 1..=n {
-        if i % 2 == 1 {
-            if i <= c {
-                b2[c-i] = deque2.pop_front().unwrap();
-            }
-            if c + i < n {
-                b2[c+i] = deque2.pop_front().unwrap();
-            }
-        }
-        else {
-            if i <= c {
-                b2[c-i] = deque2.pop_back().unwrap();
-            }
-            if c + i < n {
-                b2[c+i] = deque2.pop_back().unwrap();
-            }
-        }
-    }
-
-    let mut ans2: i64 = 0;
-    for i in 0..n-1 {
-        ans2 += (b2[i] - b2[i+1]).abs();
-    }
-
-    let ans = ans1.max(ans2);
-
+    let ans = test(n, &a_asc, &a_dsc);
+    let ans = ans.max(test(n, &a_dsc, &a_asc));
     println!("{}", ans);
 }
 
-
-use std::collections::VecDeque;
-use std::io::{self, Write};
-use std::str::FromStr;
-
-#[allow(dead_code)]
-struct Scanner {
-    stdin: io::Stdin,
-    buffer: VecDeque<String>,
-}
-#[allow(dead_code)]
+use std::io::Write; pub fn flush() { std::io::stdout().flush().unwrap(); }
+pub struct Scanner { buffer: std::collections::VecDeque<String>, buf: String }
 impl Scanner {
-    fn new() -> Self {
-        Self {
-            stdin: io::stdin(),
-            buffer: VecDeque::new(),
-        }
-    }
-
-    fn cin<T: FromStr>(&mut self) -> T {
-        while self.buffer.is_empty() {
-            let mut line = String::new();
-            let _ = self.stdin.read_line(&mut line);
-            for w in line.split_whitespace() {
-                self.buffer.push_back(String::from(w));
-            }
-        }
+    pub fn new() -> Self { Scanner { buffer: std::collections::VecDeque::new(), buf: String::new() } }
+    pub fn cin<T: std::str::FromStr>(&mut self) -> T {
+        if !self.buffer.is_empty() { return self.buffer.pop_front().unwrap().parse::<T>().ok().unwrap(); }
+        self.buf.truncate(0); std::io::stdin().read_line(&mut self.buf).ok();
+        self.buf.to_owned().split_whitespace().for_each(|x| self.buffer.push_back(String::from(x)));
         self.buffer.pop_front().unwrap().parse::<T>().ok().unwrap()
     }
-
-    fn vec<T: FromStr>(&mut self, n: usize) -> Vec<T> {
-        (0..n).map(|_| self.cin()).collect()
-    }
-}
-
-#[allow(dead_code)]
-fn flush() {
-    std::io::stdout().flush().unwrap();
+    pub fn vec<T: std::str::FromStr>(&mut self, n: usize) -> Vec<T> { (0..n).map(|_| self.cin()).collect() }
 }
