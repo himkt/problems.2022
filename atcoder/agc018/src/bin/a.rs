04611a8fbc7f53f540c8a1ad145baeb03d0e07fa
himkt <himkt@klis.tsukuba.ac.jp>
2022-09-06 08:40:12 +0900

    [AGC018] Solve A

---
+++ atcoder/agc018/src/bin/a.rs
@@ -0,0 +1,66 @@
+pub fn gcd(a: usize, b: usize) -> usize {
+    if b == 0 {
+        a
+    } else {
+        gcd(b, a % b)
+    }
+}
+
+#[allow(clippy::needless_range_loop)]
+fn main() {
+    let mut scanner = Scanner::new();
+    let n: usize = scanner.cin();
+    let k: usize = scanner.cin();
+    let a: Vec<usize> = scanner.vec(n);
+
+    let &amax = a.iter().max().unwrap();
+    if k > amax {
+        println!("IMPOSSIBLE");
+        return;
+    }
+
+    let mut gcdv = a[0];
+    for i in 1..n {
+        gcdv = gcd(gcdv, a[i]);
+    }
+
+    if k % gcdv == 0 {
+        println!("POSSIBLE");
+    }
+    else {
+        println!("IMPOSSIBLE");
+    }
+}
+
+use std::io::Write;
+pub struct Scanner {
+    buffer: std::collections::VecDeque<String>,
+    buf: String,
+}
+#[allow(clippy::new_without_default)]
+impl Scanner {
+    pub fn new() -> Self {
+        Scanner {
+            buffer: std::collections::VecDeque::new(),
+            buf: String::new(),
+        }
+    }
+    pub fn cin<T: std::str::FromStr>(&mut self) -> T {
+        if !self.buffer.is_empty() {
+            return self.buffer.pop_front().unwrap().parse::<T>().ok().unwrap();
+        }
+        self.buf.truncate(0);
+        std::io::stdin().read_line(&mut self.buf).ok();
+        self.buf
+            .to_owned()
+            .split_whitespace()
+            .for_each(|x| self.buffer.push_back(String::from(x)));
+        self.buffer.pop_front().unwrap().parse::<T>().ok().unwrap()
+    }
+    pub fn vec<T: std::str::FromStr>(&mut self, n: usize) -> Vec<T> {
+        (0..n).map(|_| self.cin()).collect()
+    }
+    pub fn flush(&self) {
+        std::io::stdout().flush().unwrap();
+    }
+}
