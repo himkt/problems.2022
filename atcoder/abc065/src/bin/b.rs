59698574d86e6257b347e2f8e35b2ac7beeb4382
himkt <himkt@klis.tsukuba.ac.jp>
2022-09-07 08:45:48 +0900

    [ABC065] Solve B

---
+++ atcoder/abc065/src/bin/b.rs
@@ -0,0 +1,54 @@
+#[allow(clippy::needless_range_loop)]
+fn main() {
+    let mut scanner = Scanner::new();
+    let n: usize = scanner.cin();
+    let a: Vec<usize> = scanner.vec::<usize>(n).iter().map(|&x| x - 1).collect();
+
+    let mut cur = 0;
+    let mut cnt = 0;
+
+    for _ in 0..n {
+        cur = a[cur];
+        cnt += 1;
+
+        if cur == 1 {
+            println!("{}", cnt);
+            return;
+        }
+    }
+
+    println!("-1");
+}
+
+use std::io::Write;
+pub struct Scanner {
+    buffer: std::collections::VecDeque<String>,
+    buf: String,
+}
+#[allow(clippy::new_without_default)]
+impl Scanner {
+    pub fn new() -> Self {
+        Scanner {
+            buffer: std::collections::VecDeque::new(),
+            buf: String::new(),
+        }
+    }
+    pub fn cin<T: std::str::FromStr>(&mut self) -> T {
+        if !self.buffer.is_empty() {
+            return self.buffer.pop_front().unwrap().parse::<T>().ok().unwrap();
+        }
+        self.buf.truncate(0);
+        std::io::stdin().read_line(&mut self.buf).ok();
+        self.buf
+            .to_owned()
+            .split_whitespace()
+            .for_each(|x| self.buffer.push_back(String::from(x)));
+        self.buffer.pop_front().unwrap().parse::<T>().ok().unwrap()
+    }
+    pub fn vec<T: std::str::FromStr>(&mut self, n: usize) -> Vec<T> {
+        (0..n).map(|_| self.cin()).collect()
+    }
+    pub fn flush(&self) {
+        std::io::stdout().flush().unwrap();
+    }
+}
