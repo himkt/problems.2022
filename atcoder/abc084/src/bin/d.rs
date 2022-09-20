a9b59668dcb456548cc3fed5902c8865e4cacaa3
himkt <himkt@klis.tsukuba.ac.jp>
2022-09-20 19:24:24 +0900

    [ABC084] Solve D

--- atcoder/abc084/src/bin/d.rs
+++ atcoder/abc084/src/bin/d.rs
@@ -1,6 +1,49 @@
+const MAX: usize = 1_000_000;
+
+pub fn eratosthenes_sieve(n: usize) -> Vec<bool> {
+    let mut s = vec![true; n];
+    s[0] = false;
+    s[1] = false;
+
+    for i in 2..n {
+        if i * i > n {
+            break;
+        }
+        if s[i] {
+            for k in 2..(n + i - 1) / i {
+                s[k * i] = false
+            }
+        }
+    }
+
+    s
+}
+
 #[allow(clippy::needless_range_loop)]
 fn main() {
     let mut scanner = Scanner::new();
+    let prime_numbers = eratosthenes_sieve(MAX);
+    let q: usize = scanner.cin();
+
+    let mut cum = vec![0; MAX + 1];
+    for ai in 1..=MAX {
+        if ai % 2 == 0 {
+            continue;
+        }
+        let bi = (ai + 1) / 2;
+        if prime_numbers[ai] && prime_numbers[bi] {
+            cum[ai] += 1;
+        }
+    }
+    for i in 1..=MAX {
+        cum[i] += cum[i - 1];
+    }
+    for _ in 0..q {
+        let l: usize = scanner.cin();
+        let r: usize = scanner.cin();
+        let v = cum[r] - cum[l - 1];
+        println!("{}", v);
+    }
 }
 
 use std::io::Write;
