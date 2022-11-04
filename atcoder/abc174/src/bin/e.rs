ed2e57e5b15ff2e244582f6be19ac0fd077b9772
himkt <himkt@klis.tsukuba.ac.jp>
2022-11-04 21:28:45 +0900

    [ABC174] re: Solve E

--- atcoder/abc174/src/bin/e.rs
+++ atcoder/abc174/src/bin/e.rs
@@ -1,17 +1,101 @@
+const MAX: usize = 1_000_000_000;
+
+fn judge(a: &[usize], mut k: usize, limit: usize) -> bool {
+    for ai in a {
+        debug!("ai={}, k={}, limit={}", ai, k, limit);
+        if ai <= &limit {
+            continue;
+        }
+        let d = ai / limit;
+        debug!("d={}", d);
+        if k < d {
+            return false;
+        }
+        k -= d;
+    }
+
+    true
+}
+
 #[allow(clippy::needless_range_loop)]
 fn main() {
     let mut scanner = Scanner::new();
-}
+    let n: usize = scanner.cin();
+    let k: usize = scanner.cin();
+    let a: Vec<usize> = scanner.vec(n);
 
-use std::io::Write; pub fn flush() { std::io::stdout().flush().unwrap(); }
-pub struct Scanner { buffer: std::collections::VecDeque<String>, buf: String }
+    let ans = lower_bound(
+        1..(MAX + 1),
+        &|x| judge(&a, k, x),
+    );
+    println!("{}", ans);
+}
+use std::io::Write;
+pub struct Scanner {
+    buffer: std::collections::VecDeque<String>,
+    buf: String,
+}
+#[allow(clippy::new_without_default)]
 impl Scanner {
-    pub fn new() -> Self { Scanner { buffer: std::collections::VecDeque::new(), buf: String::new() } }
+    pub fn new() -> Self {
+        Scanner {
+            buffer: std::collections::VecDeque::new(),
+            buf: String::new(),
+        }
+    }
     pub fn cin<T: std::str::FromStr>(&mut self) -> T {
-        if !self.buffer.is_empty() { return self.buffer.pop_front().unwrap().parse::<T>().ok().unwrap(); }
-        self.buf.truncate(0); std::io::stdin().read_line(&mut self.buf).ok();
-        self.buf.to_owned().split_whitespace().for_each(|x| self.buffer.push_back(String::from(x)));
+        if !self.buffer.is_empty() {
+            return self.buffer.pop_front().unwrap().parse::<T>().ok().unwrap();
+        }
+        self.buf.truncate(0);
+        std::io::stdin().read_line(&mut self.buf).ok();
+        self.buf
+            .to_owned()
+            .split_whitespace()
+            .for_each(|x| self.buffer.push_back(String::from(x)));
         self.buffer.pop_front().unwrap().parse::<T>().ok().unwrap()
     }
-    pub fn vec<T: std::str::FromStr>(&mut self, n: usize) -> Vec<T> { (0..n).map(|_| self.cin()).collect() }
+    pub fn vec<T: std::str::FromStr>(&mut self, n: usize) -> Vec<T> {
+        (0..n).map(|_| self.cin()).collect()
+    }
+    pub fn flush(&self) {
+        std::io::stdout().flush().unwrap();
+    }
+}
+#[macro_export]
+macro_rules! debug {
+    () => {
+        #[cfg(debug_assertions)]
+        println!();
+    };
+    ($($arg:tt)*) => {
+        #[cfg(debug_assertions)]
+        println!($($arg)*);
+    };
+}
+#[macro_export]
+macro_rules! ndarray {
+    ($x:expr;) => { $x };
+    ($x:expr; $size:expr $( , $rest:expr )*) => {
+        vec![ndarray!($x; $($rest),*); $size]
+    };
+}
+
+pub fn lower_bound(range: std::ops::Range<usize>, prop: &dyn Fn(usize) -> bool) -> usize {
+    if prop(range.start) {
+        return range.start;
+    }
+
+    let mut ng = range.start;
+    let mut ok = range.end;
+
+    while ok - ng > 1 {
+        let middle = ng + (ok - ng) / 2;
+        match prop(middle) {
+            true => ok = middle,
+            false => ng = middle,
+        }
+    }
+
+    ok
 }
